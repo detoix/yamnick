ba00c3f0076512065cdfcb70f1dba6e6ae7a007c
detoix <dk14yo@gmail.com>
2020-11-14 21:43:47 +0100

    further amqp cleanup

--- Web/src/server/index.js
+++ Web/src/server/index.js
@@ -4,4 +4,4 @@ const app = express();
 const server = http.createServer(app);
 const port = process.env.PORT || 8080;
 app.use(express.static('dist'));
-server.listen(port, () => console.log(`Listening on port ${port}`)); 
+server.listen(port, () => console.log(`Listening on port ${port}`));
--- Web/webpack.config.js
+++ Web/webpack.config.js
@@ -34,9 +34,6 @@ module.exports = {
   devServer: {
     port: 3000,
     open: true,
-    proxy: {
-      '/': 'http://localhost:8090'
-    }
   },
   plugins: [
     new CleanWebpackPlugin([outputDirectory]),
